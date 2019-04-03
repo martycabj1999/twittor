7d473e8fb30760623ae98b16308ad96ecfa819c4
martin <martingira1999@gmail.com>
2019-04-03 17:16:38 -0300

    segundo commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
+
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    if (url.includes('localhost')) {
+        swLocation = '/sw.js';
+    }
+
+    navigator.serviceWorker.register(swLocation);
 }
-
-
 
 
 // Referencias de jQuery
--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
+    //'/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
